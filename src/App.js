0a8a350bf6804c3f85d0f33147ff2158fe1c065e
Refayat Haque <refayathaque@gmail.com>
2019-05-04 23:30:12 -0400

    staged, committed and pushing to master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import "App.css";
+import ButtonAppBar from 'ButtonAppBar';
 
 // const App = () => {
 export default () => {
   return (
-    <div>
-      helloWorld
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div className="container">
+        <ButtonAppBar />
+      </div>
+    </React.Fragment>
   )
 }
 
